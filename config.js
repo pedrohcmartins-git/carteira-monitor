ea05901821cc54a893b8030a74258af4d6021528
pedrohcmartins-git <pedrohcmartins@gmail.com>
2026-03-20 17:29:32 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // ============================================================
 
 export const SUPABASE_URL  = 'https://tazqpeksxzoezcyejzaf.supabase.co'
-export const SUPABASE_ANON_KEY = 'sb_publishable_buPWMZOvopjGgBhNptSGWw_yfBdPG94'
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhenFwZWtzeHpvZXpjeWVqemFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwMzM0MTksImV4cCI6MjA4OTYwOTQxOX0.OrOtQ4dh4jMC-BJN3BSJxNnlVnHnZajxJgCDjl2rupo'
 
 // Chaves das APIs de cotação (opcionais no config, podem ser salvas no app)
 export const BRAPI_TOKEN   = '2xFQFMQ2NvEXw4B7n9kA5f'   // brapi.dev — cotações B3
